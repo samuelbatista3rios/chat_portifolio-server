5b5cd4b5af5f17c4d99cc317901d00b950f29691
Sam Fonseca <66880647+samuelbatista3rios@users.noreply.github.com>
2025-11-04 02:33:10 -0300

    Update CORS origin to include Vercel URL

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true },
+  cors: { origin: process.env.CLIENT_URL || "https://chat-portifolio-5itmlsbjh-samuelbatista3rios-projects.vercel.app" /\.vercel\.app$/,   "http://localhost:5173", credentials: true },
 });
 
 setIO(io); 
@@ -18,3 +18,4 @@ io.on("connection", (socket) => handleSocketConnection(io, socket));
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`🚀 Server rodando na porta ${PORT}`));
+
